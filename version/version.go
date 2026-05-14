8499232a32ef1ee86f533323ea2021679c1f1dfd
Or Aharonee <17099688+Aharonee@users.noreply.github.com>
2026-05-14 16:39:50 +0300

    chore(version): bump patch to 1.0.4

--- version/version.go
+++ version/version.go
@@ -20,7 +20,7 @@ const semanticAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqr
 const (
 	Major uint = 1
 	Minor uint = 0
-	Patch uint = 3
+	Patch uint = 4
 
 	// PreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
